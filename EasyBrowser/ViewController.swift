5d0ec2a36ab22e1c03a215b826746a2f884ddb21
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-03-15 04:07:30 -0300

    Added action sheet to be able to access different pages

--- EasyBrowser/ViewController.swift
+++ EasyBrowser/ViewController.swift
@@ -22,8 +22,26 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let url = URL(string: "https://www.apple.com/")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
     }
 
-
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        let url = URL(string: "https://" + action.title!)! //  Satisfy App Transport Security
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 }
 
